1a3f5f5f273e945f43d68ff5f53093477daa4309
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-06-13 13:17:45 +0000

    rotas __dir__

--- api/index.php
+++ api/index.php
@@ -1,50 +1,47 @@
+<?php
 
-<?php include './controller/processamento.php' ?>
-<?php include './view/html/head.php' ?>
-<link href="./assets/style.css" rel="stylesheet">
-    <title>ToDo List</title>
+$baseDir = dirname(__DIR__);
+
+include $baseDir . '/controller/processamento.php';
+include $baseDir . '/view/html/head.php';
+?>
+<link href="/api/assets/css/style.css" rel="stylesheet">
+<title>ToDo List</title>
 </head>
 <body>
 
-  <?php include './view/component/navbar.php' ?>
+<?php include $baseDir . '/view/component/navbar.php'; ?>
 
-  <div class="container-md mt-5">
-
+<div class="container-md mt-5">
     <h2 class="text-primary mb-4">Tarefas Cadastradas</h2>
 
-     <?php include './view/component/alert.php' ?>
+    <?php include $baseDir . '/view/component/alert.php'; ?>
 
     <div class="row border-bottom secondary-subtle mt-2">
-        <div class="col fw-bold">
-          #
-        </div>
-        <div class="col fw-bold">
-          Tarefa
-        </div>
-        <div class="col fw-bold">
-          Data
-        </div>
-        <div class="col fw-bold">
-          Ação
-        </div>
+        <div class="col fw-bold">#</div>
+        <div class="col fw-bold">Tarefa</div>
+        <div class="col fw-bold">Data</div>
+        <div class="col fw-bold">Ação</div>
     </div>
 
-    <?php foreach($_SESSION['todasTarefas'] as $tarefa): ?>
-      <div class="row border-bottom secondary-subtle  mt-2">  
-          <div class="col fw-bold"><?=$tarefa["id"];?></div>
-          <div class="col"><?=$tarefa["tarefa"];?></div>
-          <div class="col"><?=$tarefa["data"];?></div>
-          <div class="col">
-            <form method="POST" action="" style="display:inline;">
-              <input type="hidden" name="acao" value="excluir">
-              <input type="hidden" name="id" value="<?=$tarefa["id"];?>">
-              <button class="btn btn-danger"type="submit">Excluir</button>
-             </form>
-          </div>
-      </div>
-    <?php endforeach; ?>
+    <?php if (isset($_SESSION['todasTarefas']) && is_array($_SESSION['todasTarefas'])): ?>
+        <?php foreach($_SESSION['todasTarefas'] as $tarefa): ?>
+        <div class="row border-bottom secondary-subtle mt-2">
+            <div class="col fw-bold"><?=$tarefa["id"];?></div>
+            <div class="col"><?=$tarefa["tarefa"];?></div>
+            <div class="col"><?=$tarefa["data"];?></div>
+            <div class="col">
+                <form method="POST" action="" style="display:inline;">
+                    <input type="hidden" name="acao" value="excluir">
+                    <input type="hidden" name="id" value="<?=$tarefa["id"];?>">
+                    <button class="btn btn-danger" type="submit">Excluir</button>
+                </form>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
 
-  </div>
+</div>
 
-  <?php include './view/component/footer.php' ?>
-  <?php include './view/html/foot.php' ?>
+<?php include $baseDir . '/view/component/footer.php'; ?>
+<?php include $baseDir . '/view/html/foot.php'; ?>
